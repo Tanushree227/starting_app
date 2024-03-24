585d99880ab614d9e6921f4eab0acc1e5ece2f48
Tanushree227 <tanushreedas813@gmail.com>
2024-03-24 11:30:14 +0530

    styling jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Heading from "./components/Heading";
+import List from "./components/List";
 
 function App() {
   return (
     <div>
       <h1>Getting started with React.</h1>
       <Heading firstName="Bob" name="Tanushree" />
+      <List />
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <div>
+        <ul>
+            <li>{2 + 2}</li>
+            <li>{"Tanush" + 2}</li>
+            <li>{34 - 22}</li>
+        </ul>
+    </div>
+  )
+}
