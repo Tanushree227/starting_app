99cfd30483791967d88882fb926ac1bb989d92cd
Tanushree227 <tanushreedas813@gmail.com>
2024-03-24 11:34:06 +0530

    embeded jsx expressions

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 export default function List() {
+    function embedded()
+    {
+        return "22-07-1999"
+    }
   return (
     <div>
         <ul>
             <li>{2 + 2}</li>
             <li>{"Tanush" + 2}</li>
             <li>{34 - 22}</li>
+            <li>{embedded()}</li>
         </ul>
     </div>
   )
