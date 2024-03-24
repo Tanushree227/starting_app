5184303100127b5d2437614ab97b310af5f5b9e1
Tanushree227 <tanushreedas813@gmail.com>
2024-03-24 11:41:04 +0530

    embedding in attributes

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import background from './bg1.jpg';
+
+function Image(props) 
+{
+    const bg = <img src={background} alt='background' />
+    return bg;
+}
 
 export default function List() {
     function embedded()
@@ -12,6 +19,7 @@ export default function List() {
             <li>{"Tanush" + 2}</li>
             <li>{34 - 22}</li>
             <li>{embedded()}</li>
+            <Image />
         </ul>
     </div>
   )
