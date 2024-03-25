84be3bad824d614cffb3092b3e898b09cb86f1d7
Tanushree227 <tanushreedas813@gmail.com>
2024-03-25 11:42:56 +0530

    user events

--- src/components/Week2.js
+++ src/components/Week2.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Week2() {
-    function clickHandler()
-    {
-        alert("Ohh Good Job, Keep Going.")
+  let darkModeOn = false;
+  const darkMode = <h1>Dark Mode is On</h1>;
+  const lightMode = <h1>Light Mode is On</h1>;
+  function handleClick() {
+    darkModeOn = !darkModeOn;
+    if (darkModeOn === true) {
+      alert("Dark Mode is On");
+    } else {
+      alert("Light Mode is On");
     }
+  }
+  function clickHandler() {
+    alert("Ohh Good Job, Keep Going.");
+  }
 
   return (
     <div align="center">
-        <h1>Week2 Learnings</h1>
-        <button className='btn btn-success' onClick={clickHandler}>Click Here!</button>
+      <h1>Week2 Learnings</h1>
+      <button className="btn btn-success" onClick={clickHandler}>
+        Click Here!
+      </button>
+      <div>
+        {darkModeOn ? darkMode : lightMode}
+        <button className="btn btn-outline-success" onClick={handleClick}>
+          Click here again..
+        </button>
+      </div>
     </div>
-  )
+  );
 }
