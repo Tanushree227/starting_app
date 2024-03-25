46334bd37dd3e9877e451f09478eb58ae71bb605
Tanushree227 <tanushreedas813@gmail.com>
2024-03-25 11:04:46 +0530

    click events and installed bootstrap dependecy

--- src/components/Week2.js
+++ src/components/Week2.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function Week2() {
+    function clickHandler()
+    {
+        alert("Ohh Good Job, Keep Going.")
+    }
+
   return (
     <div align="center">
         <h1>Week2 Learnings</h1>
+        <button onClick={clickHandler}>Click Here!</button>
     </div>
   )
 }
