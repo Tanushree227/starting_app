806356c6f14ef6dfe0de925df0b385a7d882a99f
Tanushree227 <tanushreedas813@gmail.com>
2024-03-27 19:39:42 +0530

    installing react router dom and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import Heading from "./components/Heading";
 import List from "./components/List";
 import Week2 from "./components/Week2";
@@ -6,11 +7,12 @@ import Week3 from "./components/Week3";
 function App() {
   return (
     <div>
-      <h1>Getting started with React.</h1>
-      <Heading firstName="Bob" name="Tanushree" />
-      <List />
-      <Week2 />
-      <Week3 />
+      <Routes>
+        <Route path="/" element={<Heading firstName="Bob" name="Tanushree" />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/week-2" element={<Week2 />} />
+        <Route path="/week-3" element={<Week3 />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Heading(props) {
   return (
     <div className='d-flex justify-content-center align-items-center'>
-        <h1>Hello {props.firstName}</h1>
         <h2>Hakuna Matata {props.name}</h2>
     </div>
   )
