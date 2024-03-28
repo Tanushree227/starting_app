2e2614ec80882051ea6a36e3f6c713af1daa1f76
Tanushree227 <tanushreedas813@gmail.com>
2024-03-28 23:21:57 +0530

    more on react router

--- src/components/Week3.js
+++ src/components/Week3.js
@@ -6,6 +6,7 @@ export default function Week3() {
     <div>
         <h1>Navigation</h1>
         <p><strong>Why use React router?</strong> It allows you to build single page web applications (SPA) with navigation. React Router uses component structure to call components, which display the appropriate information.</p>
+        <p>React router is a library that allows you to handle routes in a web app, using dynamic routing. Dynamic routing takes place as the app is rendering on your machine, unlike the old routing architecture where the routing is handled in a configuration outside of a running app.</p>
     </div>
   )
 }
