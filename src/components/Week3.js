6e353075aed856f34972cd8eeb72aaa15d8b4b4c
Tanushree227 <tanushreedas813@gmail.com>
2024-03-28 23:20:32 +0530

    react router

--- src/components/Week3.js
+++ src/components/Week3.js
@@ -5,6 +5,7 @@ export default function Week3() {
   return (
     <div>
         <h1>Navigation</h1>
+        <p><strong>Why use React router?</strong> It allows you to build single page web applications (SPA) with navigation. React Router uses component structure to call components, which display the appropriate information.</p>
     </div>
   )
 }
