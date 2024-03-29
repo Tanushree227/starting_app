a1aa7ea60963bb62940f56e11ac4c1f1e0d929cf
Tanushree227 <tanushreedas813@gmail.com>
2024-03-29 10:41:36 +0530

    adding card components

--- src/components/Week3.js
+++ src/components/Week3.js
@@ -3,10 +3,12 @@ import React from 'react'
 
 export default function Week3() {
   return (
-    <div>
-        <h1>Navigation</h1>
-        <p><strong>Why use React router?</strong> It allows you to build single page web applications (SPA) with navigation. React Router uses component structure to call components, which display the appropriate information.</p>
+    <div className='d-flex justify-content-center'>
+    <div className='card shadow-lg m-5 p-5 w-75'>
+        <h1 className='card-title text-center'>Navigation</h1>
+        <p className='card-text'><strong>Why use React router?</strong> It allows you to build single page web applications (SPA) with navigation. React Router uses component structure to call components, which display the appropriate information.</p>
         <p>React router is a library that allows you to handle routes in a web app, using dynamic routing. Dynamic routing takes place as the app is rendering on your machine, unlike the old routing architecture where the routing is handled in a configuration outside of a running app.</p>
+    </div>
     </div>
   )
 }
